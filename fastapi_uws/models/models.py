7f9aaff15d7ec44af4425178852f35bf4b3a0bdd
Joshua Fraustro <jfraustro@stsci.edu>
2024-06-26 12:01:24 -0400

    types, models

---
+++ fastapi_uws/models/models.py
@@ -0,0 +1,108 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+from pydantic.fields import Field
+
+from fastapi_uws.models.types import ErrorType, ExecutionPhase, UWSVersion
+
+
+class Parameter(BaseModel):
+    """A parameter for a UWS job."""
+
+    id: str = Field(description="The identifier for the parameter.")
+    by_reference: Optional[bool] = Field(
+        default=False,
+        description="Whether the value of the parameter represents a URL to retrieve the actual parameter value.",
+    )
+    value: Optional[str] = Field(default=None, description="The value of the parameter.")
+
+
+class Parameters(BaseModel):
+    """The parameters of a job."""
+
+    parameter: list[Parameter] = Field(description="The list of parameters.")
+
+
+class ResultReference(BaseModel):
+    """A reference to a result."""
+
+    id: str = Field(description="The identifier for the result.")
+    href: Optional[str] = Field(default=None, description="The URL of the result.")
+    mime_type: Optional[str] = Field(default=None, alias="mimeType", description="The MIME type of the result.")
+    size: Optional[int] = Field(default=None, description="The size of the result in bytes.")
+
+
+class Results(BaseModel):
+    """The results of a job."""
+
+    result: list[ResultReference] = Field(description="The list of results.")
+
+
+class ErrorSummary(BaseModel):
+    """A short summary of an error."""
+
+    has_detail: bool = Field(default=False, alias="hasDetail", description="Whether a detailed error is available.")
+    message: Optional[str] = Field(default=None, description="A human-readable message describing the error.")
+    type: ErrorType = Field(description="Characterization of the type of the error.")
+
+
+class ShortJobDescription(BaseModel):
+    """A short description of a UWS job."""
+
+    creation_time: datetime = Field(alias="creationTime", description="The instant at which the job was created.")
+    href: str = Field(description="The URL of the job.")
+    job_id: str = Field(alias="jobId", description="The identifier for the job.")
+    owner_id: Optional[str] = Field(default=None, alias="ownerId", description="The owner (creator) of the job.")
+    phase: ExecutionPhase = Field(description="The current phase of the job.")
+    run_id: Optional[str] = Field(default=None, alias="runId", description="A client supplied identifier for the job.")
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        json_encoders = {datetime: lambda v: v.isoformat()}
+
+
+class JobSummary(BaseModel):
+    """The complete representation of the state of a job."""
+
+    job_id: str = Field(alias="jobId", description="The identifier for the job.")
+    run_id: Optional[str] = Field(default=None, alias="runId", description="A client supplied identifier for the job.")
+    owner_id: Optional[str] = Field(default=None, alias="ownerId", description="The owner (creator) of the job.")
+    phase: ExecutionPhase = Field(description="The current phase of the job.")
+    quote: Optional[datetime] = Field(default=None, description="When the job is likely to complete.")
+    creation_time: datetime = Field(alias="creationTime", description="The instant at which the job was created.")
+    start_time: Optional[datetime] = Field(
+        default=None, alias="startTime", description="The instant at which the job started execution."
+    )
+    end_time: Optional[datetime] = Field(
+        default=None, alias="endTime", description="The instant at which the job finished execution."
+    )
+    execution_duration: Optional[int] = Field(
+        default=None,
+        alias="executionDuration",
+        description="The duration (in seconds) for which the job should be allowed to run. 0 means unlimited.",
+    )
+    destruction_time: Optional[datetime] = Field(
+        default=None,
+        alias="destructionTime",
+        description="The time at which the job, records, and results will be destroyed.",
+    )
+    parameters: Parameters = Field(description="The parameters of the job.")
+    results: Results = Field(description="The results of the job.")
+    error_summary: Optional[ErrorSummary] = Field(
+        default=None, alias="errorSummary", description="A summary of any errors that occurred."
+    )
+    job_info: Optional[list[str]] = Field(
+        default=None, alias="jobInfo", description="Additional information about the job."
+    )
+    version: Optional[UWSVersion] = Field(
+        default=None, description="The version of the UWS standard that the job complies with."
+    )
+
+
+class Jobs(BaseModel):
+    """The list of job references returned at /jobs."""
+
+    jobref: list[ShortJobDescription] = Field(description="The list of job references.")
+    version: UWSVersion = Field(description="The version of the UWS standard that the job references comply with.")
---
+++ fastapi_uws/models/types.py
@@ -0,0 +1,87 @@
+"""UWS Simple Types"""
+
+from enum import Enum
+
+
+class ErrorType(str, Enum):
+    """Enum for error types."""
+
+    TRANSIENT = "transient"
+    """The error is transient and the job may be rerun."""
+    FATAL = "fatal"
+    """The error is fatal and the job may not be rerun."""
+
+
+class UWSVersion(str, Enum):
+    """The version of the UWS standard that the server complies with."""
+
+    V1_1 = "1.1"
+    """The server complies with UWS 1.1."""
+    V1_0 = "1.0"
+    """The server complies with UWS 1.0."""
+
+
+class ExecutionPhase(str, Enum):
+    """Enumeration of possible phases of job execution."""
+
+    PENDING = "PENDING"
+    """
+    The first phase a job is entered into - this is where a job is being set up but no request to run has occurred.
+    """
+    QUEUED = "QUEUED"
+    """
+    A job has been accepted for execution but is waiting in a queue.
+    """
+    EXECUTING = "EXECUTING"
+    """
+    A job is running
+    """
+    COMPLETED = "COMPLETED"
+    """
+    A job has completed successfully.
+    """
+    ERROR = "ERROR"
+    """
+    Some form of error has occurred.
+    """
+    UNKNOWN = "UNKNOWN"
+    """
+    The job is in an unknown state.
+    """
+    HELD = "HELD"
+    """
+    The job is HELD pending execution and will not automatically be executed.
+    Can occur after a PHASE=RUN request has been made (cf PENDING).
+    """
+    SUSPENDED = "SUSPENDED"
+    """
+    The job has been suspended by the system during execution.
+    """
+    ABORTED = "ABORTED"
+    """
+    The job has been aborted, either by user request or by the server because of lack or overuse of resources.
+    """
+    ARCHIVED = "ARCHIVED"
+    """
+    The job has been archived by the server at destruction time. An archived job may have deleted the results to reclaim
+    resources, but must have job metadata preserved. This is an alternative that the server may choose in contrast to
+    completely destroying all record of the job.
+    """
+
+
+class PhaseAction(str, Enum):
+    """Enumeration of parameters when updating the phase of a job."""
+
+    RUN = "RUN"
+    ABORT = "ABORT"
+    SUSPEND = "SUSPEND"
+    ARCHIVE = "ARCHIVE"
+
+
+class ErrorTypes(str, Enum):
+    """Enumeration of possible error types."""
+
+    TRANSIENT = "transient"
+    """The error is transient and the job may be rerun."""
+    FATAL = "fatal"
+    """The error is fatal and the job may not be rerun."""
